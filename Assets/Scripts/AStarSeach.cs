47a1dc5631fb40308e20cd04a866b8c69d32cb88
Pedro Vide Simões <pedrovide1994@hotmail.com>
2016-04-16 11:40:52 +0100

    Started a star

---
+++ Assets/Scripts/AStarSeach.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AStarSeach : SearchAlgorithm {
+
+	private HashSet<object> openSet = new HashSet<object> ();
+	private HashSet<object> closedSet = new HashSet<object> ();
+
+	// Use this for initialization
+	void Start () {
+		problem = GameObject.Find ("Map").GetComponent<Map> ().GetProblem();
+		SearchNode start = new SearchNode (problem.GetStartState (), 0);
+		openSet.Add (start.state);
+	}
+
+	protected override void Step()
+	{
+		if (openSet.Count > 0)
+		{ 
+			SearchNode cur_node = openSet [0];
+
+			for (int i = 1; i < openSet.c; i++) {
+				if (openSet[i].fcost < cur_node.fcost || openSet[i].fcost == cur_node.fcost && openSet[i].hcost < cur_node.hcost) {
+					cur_node = openSet[i];
+					}
+			}
+				openSet.Remove(cur_node.state);
+				closedSet.Add(cur_node.state);
+
+				if (problem.IsGoal (cur_node.state)) {
+					solution = cur_node;
+					finished = true;
+					running = false;
+				}
+
+				
+				Successor[] sucessors = problem.GetSuccessors (cur_node.state);
+				foreach (Successor suc in sucessors) {
+				    SearchNode new_node = new SearchNode (suc.state, suc.cost + cur_node.g, suc.action, cur_node);
+					if (closedSet.Contains (suc)) {
+						continue;
+					}
+
+				int newMovementCostToNeighbour = cur_node.gCost;
+				}
+			}
+		else
+		{
+			finished = true;
+			running = false;
+		}
+	}
+
+	int GetDistance(SearchNode A, SearchNode B){
+		int distX = A.
+	}
+		
+}
